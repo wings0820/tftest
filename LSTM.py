d58e3cc4159a379caca51a0520f4baa494b99a5a
wings0820 <6176411@qq.com>
2017-07-16 23:56:19 +0800

    7.15  23:56

--- LSTM.py
+++ LSTM.py
@@ -89,5 +89,121 @@ class PTBModel(object):
         return self._train_op
 
 class SmalloConfig(object):
-    initial_scale = 0.1
+    init_scale = 0.1
     learning_rate = 1.0
+    max_grad_norm = 5
+    num_layers = 2
+    num_steps = 20
+    hidden_size = 200
+    max_epoch = 4
+    max_max_epoch = 13
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 20
+    vocab_size = 10000
+
+class MediumConfig(object):
+    init_scale = 0.05
+    learning_rate = 1.0
+    max_grad_norm = 5
+    num_layers = 2
+    num_steps = 35
+    hidden_size = 650
+    max_epoch = 6
+    max_max_epoch = 39
+    keep_prob = 0.5
+    lr_decay = 0.8
+    batch_size = 20
+    vocab_size = 10000
+
+class LargeConfig(object):
+    init_scale = 0.04
+    learning_rate = 1.0
+    max_grad_norm = 10
+    num_layers = 2
+    num_steps = 35
+    hidden_size = 1500
+    max_epoch = 14
+    max_max_epoch = 55
+    keep_prob = 0.35
+    lr_decay = 1/1.15
+    batch_size =20
+    vocab_size = 10000
+
+class TestConfig(object):
+    init_scale = 0.1
+    learning_rate = 1.0
+    max_grad_norm = 0
+    num_layers = 1
+    num_steps = 2
+    hidden_size = 2
+    max_epoch = 1
+    max_max_epoch = 1
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size =20
+    vocab_size = 10000
+
+def run_epoch(session,model,eval_op=None,verbose = False):
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = session.run(model.initial_state)
+
+    fetched = {"cost":model.cost,"fianl_state":model.final_state,}
+    if eval_op is not None:
+        fetches["eval_op"] = eval_op
+
+    for step in range(model.input.epoch_size):
+        feed_dict = {}
+        for i,(c,h) in enumerate(model.initial_state):
+            feed_dict[c] = state[i].c
+            feed_dict[h] = state[i].h
+        vals = session.run(fetches,feed_dict)
+        cost = vals["cost"]
+        state = vals["final_state"]
+
+        cost +=cost
+        iters +=model.input.num_steps
+
+        if verbose and step %(model.input.epoch_size//10)==10:
+            print("%.3f  perplexity: %.3f speed:%.0f wps"%(step * 1.0 /model.input.epoch_size,np.exp(costs / iters),
+                                                           iters*model.input.batch_size/(time.time()-start_time)))
+    return np.exp(costs/iters)
+
+raw_data = reader.ptb_raw_data('simple-examples/data/')
+train_data,valid_data,test_data,_ = raw_data
+
+config = SmalloConfig()
+eval_config = SmalloConfig()
+eval_config.batch_size =1
+eval_config.num_steps = 1
+
+with tf.Graph().as_default():
+    initializer = tf.random_uniform_initializer(-config.init_scale,config.init_scale)
+    with tf.name_scope("Train"):
+        train_input = PTBInput(config=config,data = train_data,name = "TrainInput")
+        with tf.variable_scope("Model",reuse = None,initializer = initializer):
+            m = PTBModel(is_training=True,config=config,input_=train_input)
+
+with tf.name_scope("Valid"):
+    valid_input = PTBInput(config=config,data = valid_data,name = "ValidInput")
+    with tf.variable_scope("Model",reuse =True,initializer=initializer):
+        mvalid = PTBModel(is_training=False,config=config,input_=valid_input)
+
+with tf.name_scope("Test"):
+    test_input = PTBInput(config=eval_config,data = test_data,name = "TestInput")
+    with tf.variable_scope("Model",reuse = True,initializer = initializer):
+        mtest = PTBModel(is_training=False,config=eval_config,input_=test_input)
+
+sv = tf.train.Supervisor()
+with sv.managed_session() as session:
+    for i in range(config.max_max_epoch):
+        lr_decay = config.lr_decay**max(i+1-config.max_epoch,0.0)
+        m.assign_lr(session,config.learning_rate*lr_decay)
+        print("Epoch: %d Valid Perplexity : %.3f" % (i+1,train_perplexity))
+        valid_perplexity = run_epoch(session,mvalid)
+        print("Epoch: %d Valid Perplexity: %.3f"% (i+1,valid_perplexity))
+    test_perplexity = run_epoch(session,mtest)
+    print("Test Perplexity: %.3f "% test_perplexity)
+    
--- Word2Vec.py
+++ Word2Vec.py
@@ -80,3 +80,80 @@ def generate_batch(batch_size,num_skips,skip_window):
         buffer.append(data[data_index])
         data_index = (data_index + 1)%len(data)
     return batch,labels
+
+batch , labels = generate_batch(batch_size=8,num_skips=2,skip_window=1)
+for i in range(8):
+    print(batch[1],reverse_dictionary[batch[i]],'->',labels[i.0],reverse_dictionary[labels[i,0]])
+batch_size = 128
+embedding_size = 128
+skip_window = 1
+num_skips = 2
+valid_size = 16
+valid_window = 100
+valid_examples = np.random.choice(valid_window,valid_size,replace = False)
+num_sampled = 64
+
+graph = tf.Graph()
+with graph.as_default():
+    train_inputs = tf.placeholder(tf.int32,shape = [batch_size])
+    train_labels = tf.placeholder(tf.int32,shape = [batch_size,1])
+    valid_dataset = tf.constant(valid_examples,dtype = tf.int32)
+    with tf.device('/cpu:0'):
+        embeddings = tf.Variable(tf.random_uniform([vocabulary_size,embedding_size],-1.0,1.0))
+        embed = tf.nn.embedding_lookup(embeddings.train_inputs)
+        nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size,embedding_size],stddev=1.0/math.sqrt(embedding_size)))
+    loss = tf.reduce_mean(tf.nn.nce_loss(weights = nce_weights,biases=nce_weights,labels=train_labels,inputs=embed,num_sampled=num_sampled,num_classes=vocabulary_size))
+
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+    normalized_embeddings = embeddings / norm
+    valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings,valid_dataset)
+    similarity = tf.matmul(valid_embeddings,normalized_embeddings,transpose_b = True)
+    init = global_variables_initializer()
+
+num_steps = 100001
+with tf.Session(graph=graph) as session:
+    init.run()
+    print('Initializer')
+    average_loss = 0
+    for step in range(num_steps):
+        batch_inputs,batch_labels = generate_batch(batch_size,num_skips,skip_window)
+        feed_dict = {train_inputs:batch_inputs,train_labels:batch_labels}
+        _,loss_val = session.run([optimizer,loss],feed_dict=feed_dict)
+        average_loss+=loss_val
+
+        if step%2000 == 0:
+            if step>0:
+                average_loss/=2000
+            print("Average loss at step",step,": ",average_loss)
+            average_loss = 0
+
+        if step%1000==0:
+            sim = similarity.eval()
+            for i in range(valid_size):
+                valid_word = reserve_dictionary[valid_examples[i]]
+                top_k = 8
+                nearest = (-sim[i,:]).argsort()[1:top_k+1]
+                log_str = "Nearest to %s:"% valid_word
+                for k in range(top_k):
+                    close_word = reverse_dictionary[nearest[k]]
+                    log_str = "%s %s,"%(log_str,close_word)
+                print(log_str)
+    final_embeddings = normalized_embeddings.eval()
+
+
+def plot_with_labels(low_dim_embs,labels,filename = 'tsne.png'):
+    assert low_dim_embs.shape[0]>= len(labels),"More labels than embeddings"
+    plt.figure(figsize =(18,18))
+    for i,label in enumerate(labels):
+        x,y = low_dim_embs[i,:]
+        plt.scatter(x,y)
+        plt.annotate(label,xy=(x,y),xytext=(5,2),textcoords ='offset points',ha ='right',va = 'bottom')
+        plt.savefig(filename)
+
+from sklearn.manifold import TSNE
+import matplotlib.pylot as plt
+tsne = TSNE(perplexity=30.0,n_components=2,init = 'pca',n_iter=5000)
+plot_only = 100
+low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only,:])
+labels = [reverse_dictionary[i] for i in range(plot_only)]
+plot_with_labels(low_dim_embs,labels)
